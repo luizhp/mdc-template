adfaf759300d8ea7843ead8197df406c5109ced9
luizhp <luizhp@yahoo.com>
2019-11-04 19:12:37 -0300

    organizaçao modulo angular material

--- src/app/modules/layouts/base/components/layout-base.components.module.ts
+++ src/app/modules/layouts/base/components/layout-base.components.module.ts
@@ -1,15 +1,8 @@
-import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
 import { RouterModule } from '@angular/router';
 
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { LayoutModule } from '@angular/cdk/layout';
-
-import { MatSidenavModule } from '@angular/material/sidenav';
-import { MatToolbarModule } from '@angular/material/toolbar';
-import { MatButtonModule } from '@angular/material/button';
-import { MatIconModule } from '@angular/material/icon';
-import { MatListModule } from '@angular/material/list';
+import { MaterialModule } from './../../material.module';
 
 import { ContainerComponent } from './sidenav/container/container.component';
 import { ContentToolbarComponent } from './sidenav/content-toolbar/content-toolbar.component';
@@ -17,18 +10,8 @@ import { DrawerToolbarComponent } from './sidenav/drawer-toolbar/drawer-toolbar.
 import { DrawerMenuComponent } from './sidenav/drawer-menu/drawer-menu.component';
 import { DrawerMenuItemsComponent } from './sidenav/drawer-menu-items/drawer-menu-items.component';
 
-// import { SidenavContainerContentToolbarComponent } from './sidenav/xcontent/sidenav-container-content-toolbar/sidenav-container-content-toolbar.component';
-// import { SidenavContainerSidenavToolbarComponent } from './sidenav/xsidenav/sidenav-container-sidenav-toolbar/sidenav-container-sidenav-toolbar.component';
-// import { SidenavContainerSidenavMenuComponent } from './sidenav/xsidenav/sidenav-container-sidenav-menu/sidenav-container-sidenav-menu.component';
-// import { SidenavContainerSidenavMenuItemsComponent } from './sidenav/xsidenav/sidenav-container-sidenav-menu-items/sidenav-container-sidenav-menu-items.component';
-
 @NgModule({
     declarations: [
-        // SidenavContainerContentToolbarComponent,
-        // SidenavContainerSidenavToolbarComponent,
-        // SidenavContainerSidenavMenuComponent,
-        // SidenavContainerSidenavMenuItemsComponent,
-
         ContainerComponent,
         ContentToolbarComponent,
         DrawerToolbarComponent,
@@ -38,17 +21,12 @@ import { DrawerMenuItemsComponent } from './sidenav/drawer-menu-items/drawer-men
     ],
     imports: [
         BrowserModule,
-        BrowserAnimationsModule,
-        LayoutModule,
         RouterModule,
-        MatToolbarModule,
-        MatButtonModule,
-        MatSidenavModule,
-        MatIconModule,
-        MatListModule
+        MaterialModule,
     ],
     providers: [],
     exports: [
+        MaterialModule,
         ContainerComponent,
         ContentToolbarComponent,
         DrawerToolbarComponent,
--- src/app/modules/layouts/base/layout-base.module.ts
+++ src/app/modules/layouts/base/layout-base.module.ts
@@ -2,17 +2,9 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { RouterModule } from '@angular/router';
 
-// import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-// import { LayoutModule } from '@angular/cdk/layout';
-
-// import { MatSidenavModule } from '@angular/material/sidenav';
-// import { MatToolbarModule } from '@angular/material/toolbar';
-// import { MatButtonModule } from '@angular/material/button';
-// import { MatIconModule } from '@angular/material/icon';
-// import { MatListModule } from '@angular/material/list';
-
 import { LayoutBaseComponentsModule } from './components/layout-base.components.module';
 import { LayoutBasePageComponent } from './pages/layout-base-page/layout-base-page.component';
+
 
 @NgModule({
     declarations: [
@@ -21,18 +13,11 @@ import { LayoutBasePageComponent } from './pages/layout-base-page/layout-base-pa
     imports: [
         BrowserModule,
         RouterModule,
-        // BrowserAnimationsModule,
-        // LayoutModule,
-        // MatToolbarModule,
-        // MatButtonModule,
-        // MatSidenavModule,
-        // MatIconModule,
-        // MatListModule,
-        LayoutBaseComponentsModule
+        LayoutBaseComponentsModule,
     ],
     providers: [],
     exports: [
-        LayoutBasePageComponent
+        LayoutBasePageComponent,
     ]
 })
 export class LayoutBaseModule { }
