8b2159a57d7317d26421a77a99564a20b4d95d50
luizhp <luizhp@yahoo.com>
2019-11-08 21:31:12 -0300

    ajuste

--- src/app/modules/layouts/maua/pages/layout-maua-page/layout-maua-page.component.ts
+++ src/app/modules/layouts/maua/pages/layout-maua-page/layout-maua-page.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-//import { SidenavService } from './../../../../../services/layout/sidenav.service';
+import { SidenavService } from './../../../../../services/layout/sidenav.service';
 import { onMainContentChange } from './../../../../../animations/layouts/maua.animations';
 
 @Component({
@@ -10,15 +10,15 @@ import { onMainContentChange } from './../../../../../animations/layouts/maua.an
 })
 export class LayoutMauaPageComponent {
 
-  constructor() {}
+  //constructor() {}
 
-  // public onSideNavChange: boolean;
+  public onSideNavChange: boolean;
 
-  // constructor(private _sidenavService: SidenavService) {
-  //   this._sidenavService.sideNavState$.subscribe(res => {
-  //     console.log(res)
-  //     this.onSideNavChange = res;
-  //   })
-  // }
+  constructor(private _sidenavService: SidenavService) {
+    this._sidenavService.sideNavState$.subscribe(res => {
+      console.log(res)
+      this.onSideNavChange = res;
+    })
+  }
 
 }
