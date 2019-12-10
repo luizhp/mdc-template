cadfb1c0c7f7ce47f225fd5955a19859318abedd
luizhp <luizhp@yahoo.com>
2019-12-10 18:57:56 -0300

    limpeza ajuste profile

--- src/app/modules/layouts/maua/components/sidenav/drawer/profile/profile.component.ts
+++ src/app/modules/layouts/maua/components/sidenav/drawer/profile/profile.component.ts
@@ -1,39 +1,12 @@
 import { Component } from '@angular/core';
-//import { Component, OnDestroy } from '@angular/core';
-//import { Observable, Subscription } from 'rxjs';
-import { onSideNavChange, animateText } from './../../../../../../../animations/layouts/maua.animations';
-//import { SidenavService } from 'src/app/services/layout/sidenav.service';
 
 @Component({
   selector: 'app-mdc-maua-sidenav-drawer-profile',
   templateUrl: './profile.component.html',
-  styleUrls: ['./profile.component.css'],
-  animations: [onSideNavChange, animateText]
+  styleUrls: ['./profile.component.css']
 })
-//export class MauaSidenavDrawerProfileComponent implements OnDestroy {
 export class MauaSidenavDrawerProfileComponent {
 
-  //private subscriptions: Subscription = new Subscription();
-  //public linkText = false;
-  //[@animateText]="linkText ? 'show' : 'hide'"
-
-  constructor(
-    //private sidenavService: SidenavService
-  ) {
-    //this._startSubscriptions();
-  }
-
-  //ngOnDestroy(): void {
-    //this.subscriptions.unsubscribe();
-  //}
-
-  // private _startSubscriptions(): void {
-  //   this.subscriptions
-  //     .add(
-  //       this.sidenavService
-  //         .sideNavState$
-  //         .subscribe(x => this.linkText = x)
-  //     );
-  // }
+  constructor() { }
 
 }
