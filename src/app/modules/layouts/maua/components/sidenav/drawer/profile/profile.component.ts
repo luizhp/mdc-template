1fce3a2700ded0730eb37001221c2a734694c4c2
luizhp <luizhp@yahoo.com>
2019-12-10 18:56:47 -0300

    ajuste exibição profile

--- src/app/modules/layouts/maua/components/sidenav/drawer/profile/profile.component.ts
+++ src/app/modules/layouts/maua/components/sidenav/drawer/profile/profile.component.ts
@@ -1,9 +1,8 @@
-import { Component, OnDestroy } from '@angular/core';
-import { Observable, Subscription } from 'rxjs';
-
+import { Component } from '@angular/core';
+//import { Component, OnDestroy } from '@angular/core';
+//import { Observable, Subscription } from 'rxjs';
 import { onSideNavChange, animateText } from './../../../../../../../animations/layouts/maua.animations';
-
-import { SidenavService } from 'src/app/services/layout/sidenav.service';
+//import { SidenavService } from 'src/app/services/layout/sidenav.service';
 
 @Component({
   selector: 'app-mdc-maua-sidenav-drawer-profile',
@@ -11,28 +10,30 @@ import { SidenavService } from 'src/app/services/layout/sidenav.service';
   styleUrls: ['./profile.component.css'],
   animations: [onSideNavChange, animateText]
 })
-export class MauaSidenavDrawerProfileComponent implements OnDestroy {
+//export class MauaSidenavDrawerProfileComponent implements OnDestroy {
+export class MauaSidenavDrawerProfileComponent {
 
-  private subscriptions: Subscription = new Subscription();
-  public linkText = false;
+  //private subscriptions: Subscription = new Subscription();
+  //public linkText = false;
+  //[@animateText]="linkText ? 'show' : 'hide'"
 
   constructor(
-    private sidenavService: SidenavService,
+    //private sidenavService: SidenavService
   ) {
-    this._startSubscriptions();
+    //this._startSubscriptions();
   }
 
-  ngOnDestroy(): void {
-    this.subscriptions.unsubscribe();
-  }
+  //ngOnDestroy(): void {
+    //this.subscriptions.unsubscribe();
+  //}
 
-  private _startSubscriptions(): void {
-    this.subscriptions
-      .add(
-        this.sidenavService
-          .sideNavState$
-          .subscribe(x => this.linkText = x)
-      );
-  }
+  // private _startSubscriptions(): void {
+  //   this.subscriptions
+  //     .add(
+  //       this.sidenavService
+  //         .sideNavState$
+  //         .subscribe(x => this.linkText = x)
+  //     );
+  // }
 
 }
