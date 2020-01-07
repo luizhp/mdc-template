c7f59ba794c67c1408930adcd3537a0edda55938
luizhp <luizhp@yahoo.com>
2020-01-06 21:44:31 -0300

    hide footer, expand page - part 1

--- src/app/modules/layouts/maua/components/sidenav/content/content.component.ts
+++ src/app/modules/layouts/maua/components/sidenav/content/content.component.ts
@@ -1,24 +1,39 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy } from '@angular/core';
+import { Subscription } from 'rxjs';
+// import { onMainContentChange } from '../../../../../../animations/layouts/maua.animations';
 import { SidenavService } from '../../../../../../services/layout/sidenav.service';
-import { onMainContentChange } from '../../../../../../animations/layouts/maua.animations';
 
 @Component({
   selector: 'app-mdc-maua-sidenav-content',
   templateUrl: './content.component.html',
   styleUrls: ['./content.component.css'],
-  animations: [onMainContentChange]
+  // animations: [onMainContentChange]
 })
-export class MauaSidenavContentComponent {
+export class MauaSidenavContentComponent implements OnDestroy {
 
-  public onSideNavChange: boolean;
+  private subscriptions: Subscription = new Subscription();
+  public isSideNavVisible = true;
 
-  constructor(private sideNavService: SidenavService) {
-    this.sideNavService
-      .sideNavState$
-      .subscribe(res => {
-        console.log(res)
-        this.onSideNavChange = res;
-      });
+  constructor(
+    private sideNavService: SidenavService
+  ) {
+    this._startSubscriptions();
+  }
+
+  ngOnDestroy(): void {
+    this.subscriptions.unsubscribe();
+  }
+
+  private _startSubscriptions(): void {
+    this.subscriptions
+      .add(
+        this.sideNavService
+          .isSideNavVisible$
+          .subscribe(
+            isSideNavVisible =>
+              this.isSideNavVisible = isSideNavVisible
+          )
+      );
   }
 
 }
